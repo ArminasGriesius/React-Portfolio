8ecf25448f83616ba79aba3cfe15b0fd9f8ec952
ArminasGriesius <88167077+ArminasGriesius@users.noreply.github.com>
2024-01-17 21:22:12 +0200

    fix: hopefully fixed Tools Used section for safari

--- src/pages/InitPage.jsx
+++ src/pages/InitPage.jsx
@@ -8,7 +8,11 @@ export default function InitPage() {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
     if (isSafari) {
-      setLogoHeight("45%");
+      if (window.innerWidth >= 768) {
+        setLogoHeight("45%");
+      } else {
+        setLogoHeight("");
+      }
     } else {
       setLogoHeight("100%");
     }
@@ -32,63 +36,65 @@ export default function InitPage() {
         </section>
       </div>
       <section className={css.usedToolsGridSection}>
-        <div className={css.usedToolsFieldLeft}>
-          <h2 className={css.usedToolsTitle}>Tools used</h2>
-          <p>
-            Creating this portfolio I tried to familiarise myself with as much
-            tools as possible. I used React, Firebase, Formik, Yup, React Hot
-            Toast, Module CSS, Font Awesome Icons, Slick Carousel.
-          </p>
-        </div>
-        <div className={css.experienceFieldRight}>
-          <img
-            className={css.toolLogo}
-            src="./fotos/React.png"
-            alt="React logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/FirebaseLogo.png"
-            alt="Firebase logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/Formik.png"
-            alt="Formik logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/YupLogo.png"
-            alt="Yup logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/HotToast.png"
-            alt="Hot Toast logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/ModulesLogo.png"
-            alt="Modules Css logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/FontAwesome.png"
-            alt="Font Awesome logo"
-            style={{ height: logoHeight }}
-          />
-          <img
-            className={css.toolLogo}
-            src="./fotos/SlickLogo.png"
-            alt="Slick logo"
-            style={{ height: logoHeight }}
-          />
+        <div className={css.usedToolsField}>
+          <div className={css.usedToolsFieldLeft}>
+            <h2 className={css.usedToolsTitle}>Tools used</h2>
+            <p>
+              Creating this portfolio I tried to familiarise myself with as much
+              tools as possible. I used React, Firebase, Formik, Yup, React Hot
+              Toast, Module CSS, Font Awesome Icons, Slick Carousel.
+            </p>
+          </div>
+          <div className={css.experienceFieldRight}>
+            <img
+              className={css.toolLogo}
+              src="./fotos/React.png"
+              alt="React logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/FirebaseLogo.png"
+              alt="Firebase logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/Formik.png"
+              alt="Formik logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/YupLogo.png"
+              alt="Yup logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/HotToast.png"
+              alt="Hot Toast logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/ModulesLogo.png"
+              alt="Modules Css logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/FontAwesome.png"
+              alt="Font Awesome logo"
+              style={{ height: logoHeight }}
+            />
+            <img
+              className={css.toolLogo}
+              src="./fotos/SlickLogo.png"
+              alt="Slick logo"
+              style={{ height: logoHeight }}
+            />
+          </div>
         </div>
       </section>
     </div>
